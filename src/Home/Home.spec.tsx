7f41bcf4e8bbab52277561abe73cd0771975e1a6
Muhamad Risman <real.risman@gmail.com>
2021-04-17 02:15:18 +0700

    Add initial Home test spec

---
+++ src/Home/Home.spec.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+describe("Home", () => {
+  describe("while loading", () => {
+    it.todo("renders categories with products");
+  });
+
+  describe("with data", () => {
+    it.todo("renders categories with products");
+  });
+
+  describe("with error", () => {
+    it.todo("renders categories with products");
+  });
+});
