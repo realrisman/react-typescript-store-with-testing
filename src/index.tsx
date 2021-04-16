7061cbf8ab9d81ec911aaed859c6058c74fd5e8e
Muhamad Risman <real.risman@gmail.com>
2021-04-16 20:44:17 +0700

    Update index component to use CartProvider and BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import reportWebVitals from "./reportWebVitals";
+import { CartProvider } from "./CartContext";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
