b126b8d655c7393c12fcb902630ac8282a4c6bb7
Muhamad Risman <real.risman@gmail.com>
2021-04-17 01:47:50 +0700

    Add Header component spec

---
+++ src/shared/Header.spec.tsx
@@ -0,0 +1,18 @@
+import { fireEvent } from "@testing-library/react";
+import { Header } from "./Header";
+
+jest.mock("./CartWidget", () => ({ CartWidget: () => <div>Cart Widget</div> }));
+
+describe("Header", () => {
+  it("renders correctly", () => {
+    const { container } = renderWithRouter(() => <Header />);
+    expect(container.innerHTML).toMatch("Equipment Store");
+    expect(container.innerHTML).toMatch("Cart Widget");
+  });
+
+  it("navigates to '/' on header title click", () => {
+    const { getByText, history } = renderWithRouter(() => <Header />);
+    fireEvent.click(getByText("Equipment Store"));
+    expect(history.location.pathname).toEqual("/");
+  });
+});
