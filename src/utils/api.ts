bfaaf34cbfecbc9da9c83c74fd54233e0ad9aaf0
Muhamad Risman <real.risman@gmail.com>
2021-04-17 00:28:24 +0700

    Add submitCheckout api handler

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,22 @@
+import { CheckoutPayload } from "./../shared/types";
+
 export const getProducts = () => {
   return fetch("http://localhost:4000/products")
+    .then((res) => {
+      return res.json();
+    })
+    .catch(console.log);
+};
+
+export const submitCheckout = (data: CheckoutPayload) => {
+  return fetch("http://localhost:4000/checkout", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
     .then((res) => {
       return res.json();
     })
