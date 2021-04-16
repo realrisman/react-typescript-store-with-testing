ff5b6f98a91878b6a58e47abdc9c2934f34f87e6
Muhamad Risman <real.risman@gmail.com>
2021-04-16 20:42:46 +0700

    Add Home Component

---
+++ src/Home/Home.tsx
@@ -0,0 +1,43 @@
+import { Category } from "../shared/types";
+import { ProductCard } from "./ProductCard";
+import { useProducts } from "./useProducts";
+
+interface HomeProps {
+  useProductsHook?: () => {
+    categories: Category[];
+    isLoading: boolean;
+    error: boolean;
+  };
+}
+
+export const Home = ({ useProductsHook = useProducts }: HomeProps) => {
+  const { categories, isLoading, error } = useProductsHook();
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+
+  if (error) {
+    return <>Error</>;
+  }
+
+  return (
+    <>
+      {categories.map((category) => {
+        return (
+          <section
+            key={category.name}
+            className="nes-container with-title showcase"
+          >
+            <h2 className="title">{category.name}</h2>
+            <section className="items">
+              {category.items.map((item) => {
+                return <ProductCard key={item.name} datum={item} />;
+              })}
+            </section>
+          </section>
+        );
+      })}
+    </>
+  );
+};
