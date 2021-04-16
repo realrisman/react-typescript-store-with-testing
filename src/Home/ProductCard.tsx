8771f5e1e9f84b54b72b8ced974e59cb77bec635
Muhamad Risman <real.risman@gmail.com>
2021-04-16 20:42:25 +0700

    Add ProductCard Component

---
+++ src/Home/ProductCard.tsx
@@ -0,0 +1,38 @@
+import { Product } from "../shared/types";
+import { useCart } from "../CartContext";
+
+interface ProductProps {
+  datum: Product;
+}
+
+export const ProductCard = ({ datum }: ProductProps) => {
+  const { addToCart, products } = useCart();
+
+  const isInCart = !!products.find((product) => datum.name === product.name);
+
+  return (
+    <div className="nes-container is-rounded item">
+      <img
+        style={{ imageRendering: "pixelated" }}
+        src={datum.image}
+        width="64px"
+        height="64px"
+        alt="equipment"
+      />
+      <p>{datum.name}</p>
+      <p>{datum.price}</p>
+      {isInCart ? (
+        <button className="nes-btn is-disabled">Added to cart</button>
+      ) : (
+        <button
+          className="nes-btn is-primary"
+          onClick={() => {
+            addToCart(datum);
+          }}
+        >
+          Add to cart
+        </button>
+      )}
+    </div>
+  );
+};
