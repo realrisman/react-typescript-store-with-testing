cb5eb39999c97ffc1720ac51de6118e9c2e2c1ad
Muhamad Risman <real.risman@gmail.com>
2021-04-17 14:59:42 +0700

    Add test spec for CheckoutForm

---
+++ src/Checkout/CheckoutForm.spec.tsx
@@ -0,0 +1,13 @@
+describe("CheckoutForm", () => {
+  it.todo("renders correctly");
+
+  describe("with invalid inputs", () => {
+    it.todo("show errors");
+  });
+
+  describe("with valid inputs", () => {
+    describe("on place order button click", () => {
+      it("calls submit function with form data");
+    });
+  });
+});
