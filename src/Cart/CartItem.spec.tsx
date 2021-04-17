f7d38ddafff60e886335c59a28974e0eb11e3b55
Muhamad Risman <real.risman@gmail.com>
2021-04-17 14:32:22 +0700

    Add test CartItem renders correctly

--- src/Cart/CartItem.spec.tsx
+++ src/Cart/CartItem.spec.tsx
@@ -1,5 +1,22 @@
+import { Product } from "../shared/types";
+import { CartItem } from "./CartItem";
+
 describe("CartItem", () => {
-  it.todo("renders correctly");
+  const product: Product = {
+    name: "Product Foo",
+    price: 100,
+    image: "/image/source.png",
+  };
+
+  it("renders correctly", () => {
+    const { container, getByRole } = renderWithRouter(() => (
+      <CartItem product={product} removeFromCart={() => {}} />
+    ));
+
+    expect(container.innerHTML).toMatch("Product Foo");
+    expect(container.innerHTML).toMatch("100 Zm");
+    expect(getByRole("img")).toHaveAttribute("src", "/image/source.png");
+  });
 
   describe("on 'Remove' click", () => {
     it.todo("calls passed in function");
