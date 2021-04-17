ee4423c10416548d0b24fa6d17ae986f10ebddf0
Muhamad Risman <real.risman@gmail.com>
2021-04-17 15:47:21 +0700

    Add test CheckoutForm with invalid inputs

--- src/Checkout/CheckoutForm.spec.tsx
+++ src/Checkout/CheckoutForm.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { CheckoutForm } from "./CheckoutForm";
 
 describe("CheckoutForm", () => {
@@ -12,7 +13,18 @@ describe("CheckoutForm", () => {
   });
 
   describe("with invalid inputs", () => {
-    it.todo("show errors");
+    it("show errors", async () => {
+      const { container, getByText } = render(<CheckoutForm />);
+
+      await act(async () => {
+        fireEvent.click(getByText("Place order"));
+      });
+
+      expect(container.innerHTML).toMatch("name is a required field");
+      expect(container.innerHTML).toMatch("cardNumber is a required field");
+      expect(container.innerHTML).toMatch("expDate is a required field");
+      expect(container.innerHTML).toMatch("cvv is a required field");
+    });
   });
 
   describe("with valid inputs", () => {
