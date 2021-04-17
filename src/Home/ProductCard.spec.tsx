13cdb11e07171258924801b86a6a9eef5fb0b0ef
Muhamad Risman <real.risman@gmail.com>
2021-04-17 13:23:20 +0700

    Initial ProductCard test spec

---
+++ src/Home/ProductCard.spec.tsx
@@ -0,0 +1,13 @@
+describe("ProductCard", () => {
+  it.todo("renders correctly");
+
+  describe("when product is in the cart", () => {
+    it.todo("the 'Add to cart' button is disabled");
+  });
+
+  describe("when product is not in the cart", () => {
+    describe("on 'Add to cart' click", () => {
+      it("calls 'addToCard' function");
+    });
+  });
+});
