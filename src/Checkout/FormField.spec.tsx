31286713858b9261b1f0a8aace7cbef3707dc596
Muhamad Risman <real.risman@gmail.com>
2021-04-21 12:59:12 +0700

    Add initial FormField test spec

---
+++ src/Checkout/FormField.spec.tsx
@@ -0,0 +1,11 @@
+describe("FormField", () => {
+  it.todo("renders correctly");
+
+  describe("with error", () => {
+    it.todo("render error message");
+  });
+
+  describe("on change", () => {
+    it.todo("normalizes the input");
+  });
+});
