a6b3270d152c2c07d62aa10e3152c840627a1283
Muhamad Risman <real.risman@gmail.com>
2021-04-21 14:38:32 +0700

    Add all test OrderSumarry

--- src/OrderSummary/OrderSummary.spec.tsx
+++ src/OrderSummary/OrderSummary.spec.tsx
@@ -1,17 +1,72 @@
+import { fireEvent, render } from "@testing-library/react";
+import { Loader } from "../shared/Loader";
+import { OrderSummary } from "./OrderSummary";
+
+jest.mock("../shared/Loader", () => ({
+  Loader: jest.fn(() => null),
+}));
+
 describe("OrderSummary", () => {
   afterEach(jest.clearAllMocks);
 
   describe("while order data being loaded", () => {
+    // it("renders loader", () => {
+    //   const stubUseOrder = () => ({
+    //     isLoading: true,
+    //     order: undefined,
+    //   });
+    //   render(<OrderSummary useOrderHook={stubUseOrder} />);
+    //   expect(Loader).toHaveBeenCalled();
+    // });
+
     it.todo("renders loader");
   });
 
   describe("when order is loaded", () => {
-    it.todo("renders order info");
+    const stubUseOrder = () => ({
+      isLoading: false,
+      order: {
+        products: [
+          {
+            name: "Diamond Sword",
+            price: 1000,
+            image: "image.png",
+          },
+        ],
+      },
+    });
 
-    it.todo("navigates to main page on button click");
+    it("renders order info", () => {
+      const { container } = renderWithRouter(() => (
+        <OrderSummary useOrderHook={stubUseOrder} />
+      ));
+
+      expect(container.innerHTML).toMatch("Diamond Sword");
+    });
+
+    it("navigates to main page on button click", () => {
+      const { getByText, history } = renderWithRouter(() => (
+        <OrderSummary useOrderHook={stubUseOrder} />
+      ));
+
+      fireEvent.click(getByText("Back to the store"));
+
+      expect(history.location.pathname).toEqual("/");
+    });
   });
 
   describe("without order", () => {
-    it.todo("renders error message");
+    it("renders error message", () => {
+      const stubUseOrder = () => ({
+        isLoading: false,
+        order: undefined,
+      });
+
+      const { container } = render(
+        <OrderSummary useOrderHook={stubUseOrder} />
+      );
+
+      expect(container.innerHTML).toMatch("Couldn't load order info.");
+    });
   });
 });
