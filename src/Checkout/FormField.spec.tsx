c450ec7cd2445b349474f641aa9354327c56a93e
Muhamad Risman <real.risman@gmail.com>
2021-04-21 13:02:34 +0700

    Add test renders correctly FormField

--- src/Checkout/FormField.spec.tsx
+++ src/Checkout/FormField.spec.tsx
@@ -1,5 +1,16 @@
+import { render } from "@testing-library/react";
+import { FormField } from "./FormField";
+
 describe("FormField", () => {
-  it.todo("renders correctly");
+  it("renders correctly", () => {
+    const { getByLabelText } = render(
+      <FormField label="Foo label" name="foo" />
+    );
+    const input = getByLabelText("Foo label");
+    expect(input).toBeInTheDocument();
+    expect(input).not.toHaveClass("is-error");
+    expect(input).toHaveAttribute("name", "foo");
+  });
 
   describe("with error", () => {
     it.todo("render error message");
